23f825b528a504b7b0d88cf5394885de15dd2ef1
NMeJa <52712676+NMeJa@users.noreply.github.com>
2023-06-13 23:10:04 +0400

    Update Beautify.cs

--- NMJToolBox/NMJ/Runtime/Extensions/Beautify.cs
+++ NMJToolBox/NMJ/Runtime/Extensions/Beautify.cs
@@ -1,30 +1,33 @@
 using System.Text.RegularExpressions;
 using System.Globalization;
 
-public static class StringBeautifier
+namespace NMJToolBox
 {
-	public static string CapitalizeFirstLetter(this string s)
+	public static class StringBeautifier
 	{
-		return string.IsNullOrEmpty(s) ? string.Empty : new CultureInfo("en-US", false).TextInfo.ToTitleCase(s.ToLower());
-	}
+		public static string CapitalizeFirstLetter(this string s)
+		{
+			return string.IsNullOrEmpty(s) ? string.Empty : new CultureInfo("en-US", false).TextInfo.ToTitleCase(s.ToLower());
+		}
 
-	public static string RemoveExcessWhitespace(this string s)
-	{
-		return string.IsNullOrEmpty(s) ? string.Empty : Regex.Replace(s, @"\s+", " ");
-	}
+		public static string RemoveExcessWhitespace(this string s)
+		{
+			return string.IsNullOrEmpty(s) ? string.Empty : Regex.Replace(s, @"\s+", " ");
+		}
 
-	public static string ReplaceUnderscoresWithSpaces(this string s)
-	{
-		return string.IsNullOrEmpty(s) ? string.Empty : s.Replace("_", " ");
-	}
+		public static string ReplaceUnderscoresWithSpaces(this string s)
+		{
+			return string.IsNullOrEmpty(s) ? string.Empty : s.Replace("_", " ");
+		}
 
-	public static string SplitCamelCase(this string s)
-	{
-		return string.IsNullOrEmpty(s) ? string.Empty : Regex.Replace(s, "([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))", "$1 ").Trim();
-	}
+		public static string SplitCamelCase(this string s)
+		{
+			return string.IsNullOrEmpty(s) ? string.Empty : Regex.Replace(s, "([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))", "$1 ").Trim();
+		}
 
-	public static string Beautify(this string s)
-	{
-		return string.IsNullOrEmpty(s) ? string.Empty : s.ReplaceUnderscoresWithSpaces().SplitCamelCase().CapitalizeFirstLetter().RemoveExcessWhitespace();
+		public static string Beautify(this string s)
+		{
+			return string.IsNullOrEmpty(s) ? string.Empty : s.ReplaceUnderscoresWithSpaces().SplitCamelCase().CapitalizeFirstLetter().RemoveExcessWhitespace();
+		}
 	}
 }
