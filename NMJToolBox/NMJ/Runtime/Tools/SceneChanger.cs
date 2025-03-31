9e53088b8c654914260837c9065b575c26aaefa5
NMJ <52712676+NMeJa@users.noreply.github.com>
2025-04-01 00:04:36 +0400

    Update SceneChanger.cs

--- NMJToolBox/NMJ/Runtime/Tools/SceneChanger.cs
+++ NMJToolBox/NMJ/Runtime/Tools/SceneChanger.cs
@@ -1,39 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-#if ODIN_INSPECTOR
-using Sirenix.OdinInspector;
-#endif
 
 namespace NMJToolBox
 {
-	public class SceneChanger : MonoBehaviour, ISerializationCallbackReceiver
+	public class SceneChanger : MonoBehaviour
 	{
 #if UNITY_EDITOR
 		[SerializeField]
 		private UnityEditor.SceneAsset sceneAsset;
 #endif
 
-
-#if ODIN_INSPECTOR
-	[ReadOnly]
-#endif
 		[SerializeField]
-		private string sceneName;
+		private string scenePath;
+	        public string ScenePath
+	        {
+	            get => scenePath;
+	            set => scenePath = value;
+	        }
 
 		public void _LoadScene(string scene) => SceneManager.LoadScene(scene);
 		public void _LoadScene() => SceneManager.LoadScene(sceneName);
 		public AsyncOperation _LoadParallelScene() => SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
 		public AsyncOperation _LoadParallelScene(LoadSceneMode mode) => SceneManager.LoadSceneAsync(sceneName, mode);
-
 		public void _UnLoadParallelScene() => SceneManager.UnloadSceneAsync(sceneName, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
-
-		public void OnBeforeSerialize()
-		{
-#if UNITY_EDITOR
-			if (sceneAsset) sceneName = UnityEditor.AssetDatabase.GetAssetOrScenePath(sceneAsset);
-#endif
-		}
-
-		public void OnAfterDeserialize() { }
 	}
 }
