98463aa95cc7584536395c3faed9165c191843c6
NMJ <52712676+NMeJa@users.noreply.github.com>
2025-04-01 00:29:53 +0400

    Update SceneChanger.cs

--- NMJToolBox/NMJ/Runtime/Tools/SceneChanger.cs
+++ NMJToolBox/NMJ/Runtime/Tools/SceneChanger.cs
@@ -3,25 +3,26 @@ using UnityEngine.SceneManagement;
 
 namespace NMJToolBox
 {
-	public class SceneChanger : MonoBehaviour
-	{
+    public class SceneChanger : MonoBehaviour
+    {
 #if UNITY_EDITOR
-		[SerializeField]
-		private UnityEditor.SceneAsset sceneAsset;
+        [SerializeField]
+        private UnityEditor.SceneAsset sceneAsset;
 #endif
-		[UnityEngine.Serialization.FormerlySerializedAs("sceneName")]
-		[SerializeField]
-		private string scenePath;
-	        public string ScenePath
-	        {
-	            get => scenePath;
-	            set => scenePath = value;
-	        }
+        [UnityEngine.Serialization.FormerlySerializedAs("sceneName")]
+        [SerializeField]
+        private string scenePath;
+        public string ScenePath
+        {
+            get => scenePath;
+            set => scenePath = value;
+        }
+        public bool IsActiveScene => SceneManager.GetActiveScene().path.Equals(scenePath);
 
-		public void _LoadScene(string scene) => SceneManager.LoadScene(scene);
-		public void _LoadScene() => SceneManager.LoadScene(scenePath);
-		public AsyncOperation _LoadParallelScene() => SceneManager.LoadSceneAsync(scenePath, LoadSceneMode.Additive);
-		public AsyncOperation _LoadParallelScene(LoadSceneMode mode) => SceneManager.LoadSceneAsync(scenePath, mode);
-		public void _UnLoadParallelScene() => SceneManager.UnloadSceneAsync(scenePath, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
-	}
+        public void _LoadScene(string scene) => SceneManager.LoadScene(scene);
+        public void _LoadScene() => SceneManager.LoadScene(scenePath);
+        public AsyncOperation _LoadParallelScene() => SceneManager.LoadSceneAsync(scenePath, LoadSceneMode.Additive);
+        public AsyncOperation _LoadParallelScene(LoadSceneMode mode) => SceneManager.LoadSceneAsync(scenePath, mode);
+        public void _UnLoadParallelScene() => SceneManager.UnloadSceneAsync(scenePath, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+    }
 }
